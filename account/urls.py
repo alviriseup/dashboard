c5e59eefa9ae2da16a85a678c28167e4c7392ba2
alviriseup <alvi@riseuplabs.com>
2024-09-11 14:50:10 +0600

    added name and password change views

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import user_profile
+from .views import *
 
 urlpatterns = [
     path('profile/', user_profile, name='user-profile'),
+    path('profile/update_user_info', update_user_info, name='update-user-info'),
+    path('profile/update_password', update_password, name='update-password'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from .forms import UpdateUserForm, UpdatePasswordForm
 
 
 @login_required
@@ -8,6 +11,49 @@ def user_profile(request):
     context = {
         'full_name': f'{user.first_name} {user.last_name}',
         'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
         'email': user.email,
     }
     return render(request, 'account/user_profile.html', context)
+
+
+
+# Update user first name and last name
+@login_required
+def update_user_info(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'Your profile has been updated successfully!')
+            return redirect('user-profile')
+        else:
+            messages.error(request, 'Error updating profile info.')
+
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+
+    return render(request, 'account/user_profile.html', {'user_form': user_form})
+
+
+
+# Update user password
+@login_required
+def update_password(request):
+    if request.method == 'POST':
+        password_form = UpdatePasswordForm(user=request.user, data=request.POST)
+
+        if password_form.is_valid():
+            user = password_form.save()
+            update_session_auth_hash(request, user)     # Keeps user logged in after password change
+            messages.success(request, 'Your password has been updated successfully!')
+            return redirect('user-profile')
+        else:
+            messages.error(request, 'Error updating password.')
+
+    else:
+        password_form = UpdatePasswordForm(user=request.user)
+
+    return render(request, 'account/user_profile.html', {'password_form': password_form})
