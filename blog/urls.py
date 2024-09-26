166691b1205cac61f9452053060c2c07b395e91a
alviriseup <alvi@riseuplabs.com>
2024-09-26 14:36:08 +0600

    refactoring landing page

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_list, name="blog_list"),
-    path("blog_index/", views.blog_index, name="blog_index"),
+    # path("blog_index/", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
     path("create/", views.create_blog_post, name="create_blog_post"),
