d7e6d897feeadf94bb8532c505ca210e6c1ce3f9
alviriseup <alvi@riseuplabs.com>
2024-10-20 17:57:07 +0600

    Added select2 for categories selection

--- blog/forms.py
+++ blog/forms.py
@@ -21,10 +21,16 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'body', 'categories']
-        widgets = {
-            # 'categories': forms.CheckboxSelectMultiple,
-            'categories': forms.SelectMultiple,
-        }
+        # widgets = {
+        #     # 'categories': forms.CheckboxSelectMultiple,
+        #     'categories': forms.SelectMultiple,
+        #     # 'categories': forms.SelectMultiple(attrs={
+        #     #     'class': 'form-control select2-multiple',
+        #     #     'data-toggle': 'select2',
+        #     #     'multiple': 'multiple',
+        #     #     'data-placeholder': 'Choose categories...'
+        #     # }),
+        # }
         
 
 class CategoryForm(forms.ModelForm):
